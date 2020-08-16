ca5efa318274743276c61cdcf1fad409a2f66779
0x4c43-hub <1476696961@qq.com>
2020-08-16 22:07:56 +0800

    git

---
+++ git.c
@@ -0,0 +1,11 @@
+git config --global user.name "[name]"
+git config --global user.email "[email]"
+git init
+git init [project-name]
+git clone [url]
+git status
+git add [filename]/[dir]
+git commit -m "message"
+git remote add [remote] [url]
+git pull --rebase [remote] [branch]
+git push [remote] [branch]
